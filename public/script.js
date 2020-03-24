08b7f20d0a45e3075f9ea0f508d7066c79a2b94e
Souriya Phongsavanh <souriya.phongsavanh777@gmail.com>
2020-03-24 17:37:01 +0100

    save

--- public/script.js
+++ public/script.js
@@ -1,1 +1,63 @@
-console.log("It works!");
+new Vue({
+	el: "#app",
+	data: {
+    total: 0,
+		products: [
+			{ id: 1, title: "Product 1", price: 3.99 },
+			{ id: 2, title: "Product 2", price: 4 },
+			{ id: 3, title: "Product 3", price: 5.99 },
+		],
+		cart: []
+	},
+	methods: {
+		addToCart: function(product) {
+      this.total += product.price;
+      const cartItem = this.cart.find(({ id }) => id === product.id);
+      if (cartItem) {
+        cartItem.qty++
+      } else {
+        this.cart.push({
+          id: product.id,
+          title: product.title,
+          price: product.price,
+          qty: 1
+        });
+      }
+    },
+    inc: function(item) {
+      item.qty++;
+      this.total += item.price;
+    },
+    dec: function(item) {
+      if(item.qty === 1) {
+        this.cart = this.cart.filter(({ id }) => id !== item.id)
+      } else {
+        item.qty--;
+        this.total -= item.price;
+      }
+    },
+  },
+  // computed: {
+  //   total: function() {
+  //     if(this.cart.length > 0) {
+  //       return this.cart.reduce((acc, current) => acc + current.price)
+  //     } else {
+  //       return 0;
+  //     }
+  //   }
+  // },
+  // watch: {
+  //   cart: function(val) {
+  //     const cartItem = this.cart.filter(({ id }) => id === val.id);
+  //     console.log(cartItem.price)
+  //     if(cartItem) {
+  //       cartItem.price = cartItem.qty * cartItem.price;
+  //     }
+  //   }
+  // },
+  filters: {
+    currency: function(price) {
+      return '$' + price.toFixed(2)
+    }
+  }
+});
